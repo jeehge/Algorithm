ba2fc69cfb6f52fb3fbabf27d5659ca22208bc4b
JH <taerg89@gmail.com>
2021-04-20 01:15:16 +0900

    프로그래머스 문제 - 위장

--- Algorithm.playground/Contents.swift
+++ Algorithm.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
-var str = "Hello, playground"
+func solution(_ clothes: [[String]]) -> Int {
+	var dictionary: [String: [String]] = [:]
+	var answer = 1
+	for item in clothes {
+		if dictionary.keys.contains(item[1]) {
+			dictionary[item[1]]!.append(item[0])
+		} else {
+			dictionary[item[1]] = [item[0]]
+		}
+	}
+	let keys = Array(dictionary.keys)
+	for j in 0..<keys.count {
+		answer *= dictionary[keys[j]]!.count + 1
+	}
+	return answer - 1
+}
+
+let clothes = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"],["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]]
+
+solution(clothes)
