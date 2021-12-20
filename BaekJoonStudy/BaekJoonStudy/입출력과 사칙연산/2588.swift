740d58faf9431673d1d4fd11552e517f27f71187
JH <taerg89@gmail.com>
2021-12-20 12:13:28 +0900

    Add 2588 solution 2

--- BaekJoonStudy/BaekJoonStudy/입출력과 사칙연산/2588.swift
+++ BaekJoonStudy/BaekJoonStudy/입출력과 사칙연산/2588.swift
@@ -11,6 +11,21 @@ class solution2588: ResultPresentable {
     func resultPrint() {
         let input = readLine()!
         let inputArr = input.split(separator: " ")
+        
+//        let a = Int(inputArr[0])!
+//        let b = Int(inputArr[1])!
+//        
+//        let location = String(b).map { Int(String($0))! }
+//        var result: Int = 0
+//        
+//        location.reversed().enumerated().forEach {
+//            let resultPrint = a * Int($1)
+//            print(resultPrint)
+//            
+//            result += (resultPrint * Int(pow(Double(10), Double($0))))
+//        }
+//        print(result)
+        
         let a = Int(inputArr[0])!
         let b = Int(inputArr[1])!
         
