4251744a09aa65e02c936d44cdf499e2121af4ce
JH <taerg89@gmail.com>
2022-01-03 00:41:38 +0900

    Add 11022 solution

---
+++ BaekJoonStudy/BaekJoonStudy/for문/11022.swift
@@ -0,0 +1,22 @@
+//
+//  11022.swift
+//  BaekJoonStudy
+//
+//  Created by JH on 2022/01/03.
+//
+
+import Foundation
+
+class solution11022: ResultPresentable {
+    func resultPrint() {
+        let input = Int(readLine()!)!
+
+        for index in 1...input {
+            let number = readLine()!
+            let numberArray = number.split(separator: " ")
+            let a = Int(numberArray[0])!
+            let b = Int(numberArray[1])!
+            print("Case #\(index): \(a) + \(b) = \(a + b)")
+        }
+    }
+}
